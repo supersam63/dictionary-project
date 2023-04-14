40e1d60819d2d92e7ba0caaac48688d9153f790d
supersam63 <113955440+supersam63@users.noreply.github.com>
2023-04-14 16:36:59 -0600

    added axios

---
+++ src/Dictionary.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    const apiKey = "4a240de8db217dtodb6166f343d5aa4a";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordCange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordCange} autoFocus={true} />
+      </form>
+    </div>
+  );
+}
