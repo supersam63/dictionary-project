b0ff851b49b6946fcb9c496030b64e2516b874c8
supersam63 <113955440+supersam63@users.noreply.github.com>
2023-04-18 09:58:35 -0600

    created synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -10,6 +11,7 @@ export default function Meaning(props) {
       <p>
         <em>{props.meaning.example}</em>
       </p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
