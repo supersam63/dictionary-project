d5a0681e27b42119d67d2862ac0caeb503c5da67
supersam63 <113955440+supersam63@users.noreply.github.com>
2023-04-18 10:41:13 -0600

    added phonetics

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,8 +8,6 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
-
     setResults(response.data);
   }
 
